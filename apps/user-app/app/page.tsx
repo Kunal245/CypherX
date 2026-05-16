7512357d34f18f517641de66574d343016872254
Kunal <killerkunal45@gmail.com>
2026-05-16 23:29:24 +0000

    updated root page.tsx

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -14,16 +14,16 @@
 // }
 
 
-"use client"
-import { Appbar } from "@/components/Appbar";
-import { signIn, signOut, useSession } from "next-auth/react";
-import { JSX } from "react";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { redirect } from 'next/navigation'
 
-export default function Page(): JSX.Element {
-  const session = useSession();
-  return (
-   <div>
-      {/* <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} /> */}
-   </div>
-  );
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+  if (session?.user) {
+    redirect('/dashboard')
+  } else {
+    redirect('/api/auth/signin')
+  }
+  
 }
