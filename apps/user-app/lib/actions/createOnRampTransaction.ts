160cc07c2cc1afcec9cbd5e027763aee9bddafad
Kunal <killerkunal45@gmail.com>
2026-05-18 10:31:53 +0000

    On ramp txn added

--- apps/user-app/lib/actions/createOnRampTransaction.ts
+++ apps/user-app/lib/actions/createOnRampTransaction.ts
@@ -1,9 +1,43 @@
 
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth";
+import { redirect } from "next/navigation";
+import { db } from "@workspace/db/client";
+
+
 
 //ps- creating function to put data in db when server call happenn on button click
 //we don't use axios call as this is monorepo which gives ability to directly fetch data from db at server-side
-
 //TRANSFER PAGE- ADD MONEY
 //ON-BUTTON CLICK
 
+export async function createOnRampTransaction(amount: number, provider: string) {
+    //onClick i/p- user sends amount and bank provider 
+    // **don't send userId rather extract it from NextAuth- cause security risk**
 
+    const session = await getServerSession(authOptions)
+    const userId = session?.user?.id
+    const token = Math.random().toString()
+
+    if(!userId) {
+        redirect("api/auth/signin")
+    }
+
+    //if user exist store txn data
+    await db.onRampTransaction.create({
+        data: {
+            userId,
+            amount: amount,
+            status: "Processing",
+            startTime: new Date(),
+            provider: provider,
+            token: token
+        }
+    })
+
+    return {
+        msg: "On Ramp Transaction added"
+    }
+
+    
+}
