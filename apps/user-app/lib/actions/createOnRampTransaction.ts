96895d282a239133e26e93ec573fc9bdca2af343
Kunal <killerkunal45@gmail.com>
2026-05-18 10:10:14 +0000

    adding onRamp button fn(server-side)

---
+++ apps/user-app/lib/actions/createOnRampTransaction.ts
@@ -0,0 +1,9 @@
+
+
+//ps- creating function to put data in db when server call happenn on button click
+//we don't use axios call as this is monorepo which gives ability to directly fetch data from db at server-side
+
+//TRANSFER PAGE- ADD MONEY
+//ON-BUTTON CLICK
+
+
