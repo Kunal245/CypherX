daf6ac94ff0101047ef5dba3bb13808deb390b2b
Kunal <killerkunal45@gmail.com>
2026-05-19 13:52:24 +0000

    styled p2pcard

--- apps/user-app/components/P2pCard.tsx
+++ apps/user-app/components/P2pCard.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 
+import { p2pTransfer } from "@/lib/actions/p2pTransfer";
 import { Button } from "@workspace/ui/components/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@workspace/ui/components/card";
 import { Input } from "@workspace/ui/components/input";
@@ -15,26 +16,26 @@ export function P2pCard() {
     const [amount, setAmount] = useState("")
 
 
-    return <div>
-        <Card className="h-fit">
+    return <div className="flex justify-center items-center min-h-96">
+        <Card className="w-full max-w-sm">
             <CardHeader>
                 <CardTitle>Send</CardTitle>
             </CardHeader>
-            <CardContent>
-                <div>
+            <CardContent className="space-y-4" >
+                <div className="space-y-2" >
                     <Label>Number</Label>
                     <Input placeholder="Enter Number" onChange={(e) => {
                         setNumber(e.target.value)
                     }} ></Input>
                 </div>
-                <div>
+                <div className="space-y-2" >
                     <Label>Amount</Label>
                     <Input placeholder="Enter Amount" onChange={(e) => {
                         setAmount(e.target.value)
                     }} ></Input>
                 </div>
-                <Button onClick={() => {
-
+                <Button className="w-full" onClick={async () => {
+                    await p2pTransfer(number, Number(amount))
                 }}>Send</Button>
             </CardContent>
         </Card>
