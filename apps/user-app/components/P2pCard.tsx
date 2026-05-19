b2e1ae593f6e7fd2a48669882fd0b546fd9bdd47
Kunal <killerkunal45@gmail.com>
2026-05-19 10:51:34 +0000

    added state variables

--- apps/user-app/components/P2pCard.tsx
+++ apps/user-app/components/P2pCard.tsx
@@ -2,10 +2,16 @@ import { Button } from "@workspace/ui/components/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@workspace/ui/components/card";
 import { Input } from "@workspace/ui/components/input";
 import { Label } from "@workspace/ui/components/label";
+import { useState } from "react";
 
 
 
 export function P2pCard() {
+
+    const [number, setNumber] = useState("")
+    const [amount, setAmount] = useState("")
+
+
     return <div>
         <Card className="h-fit">
             <CardHeader>
