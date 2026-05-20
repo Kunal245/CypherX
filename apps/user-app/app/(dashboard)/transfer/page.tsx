3ff0f17d7bb418aade8a7ca4efbcea25c0b5c188
Kunal <killerkunal45@gmail.com>
2026-05-20 09:20:43 +0000

    adding p2p transaction card

--- apps/user-app/app/(dashboard)/transfer/page.tsx
+++ apps/user-app/app/(dashboard)/transfer/page.tsx
@@ -1,59 +1,17 @@
 
 
 
-import { db } from "@workspace/db/client";
+import { getBalance } from "@/lib/actions/getBalance";
 import { AddMoney } from "../../../components/AddMoneyCard";
 import { BalanceCard } from "../../../components/BalanceCard";
 import { OnRampTransactions } from "../../../components/OnRampTransaction";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/auth";
-import { redirect } from "next/navigation";
+import { getOnRampTransactions } from "@/lib/actions/getOnRampTransaactions";
 
-async function getBalance() {
-  const session = await getServerSession(authOptions);
-
-
-  //********BUG******
-  //even afterr clicking logout it tries to fetch balance from db which,
-  //became undefined after logout
-  //so we need to add this logic to redirect the user to the signin page if the session is expired 
-  if(!session?.user?.id) {
-    redirect("api/auth/signin")
-  }
-  //FIXED
-
-    // console.log(session);
-    // console.log("debugginggg");
-
-  const balance = await db.balance.findFirst({
-    where: { userId: Number(session?.user?.id) },
-    //Number(session?.user?.id)- this means if(session) get(user) & if(user) get(id)
-    //why "?": this means if session= null it crashes but if used "?" it gives undefined
-    //And wwhen done Number(undefined) give "NaN" which prisma rejects and not crash 
-  });
-  return {
-    amount: balance?.amount || 0,
-    locked: balance?.locked || 0,
-  };
-}
-
-async function getOnRampTransactions() {
-  const session = await getServerSession(authOptions);
-  const txns = await db.onRampTransaction.findMany({
-    where: { userId: Number(session?.user?.id) },
-  });
-  return txns.map((t) => ({
-    time: t.startTime,
-    amount: t.amount,
-    status: t.status,
-    provider: t.provider,
-  }));
-}
 
 export default async function TransferPage() {
   const balance = await getBalance();
   const transactions = await getOnRampTransactions();
-
+  
   return (
     <div className="p-6 max-w-screen-xl mx-auto">
       <h1 className="text-3xl font-bold mb-8">Transfer</h1>
---
+++ apps/user-app/lib/actions/getOnRampTransaactions.ts
@@ -0,0 +1,21 @@
+"use server"
+
+
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth";
+import { db } from "@workspace/db/client";
+
+
+
+export async function getOnRampTransactions() {
+  const session = await getServerSession(authOptions);
+  const txns = await db.onRampTransaction.findMany({
+    where: { userId: Number(session?.user?.id) },
+  });
+  return txns.map((t) => ({
+    time: t.startTime,
+    amount: t.amount,
+    status: t.status,
+    provider: t.provider,
+  }));
+}
