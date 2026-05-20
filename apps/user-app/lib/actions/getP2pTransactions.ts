c30edb8e0bcf6a05563e1729dd640246091581e5
Kunal <killerkunal45@gmail.com>
2026-05-20 09:41:48 +0000

    added getP2pTransaction server action

---
+++ apps/user-app/lib/actions/getP2pTransactions.ts
@@ -0,0 +1,22 @@
+"use server"
+
+
+
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth";
+import { db } from "@workspace/db/client";
+
+
+
+export async function getP2pTransaction() {
+  const session = await getServerSession(authOptions);
+  const txns = await db.p2pTransactions.findMany({
+    where: { senderId: Number(session?.user?.id) },
+  });
+  return txns.map((t) => ({
+    time: t.timestamp,
+    amount: t.amount,
+    sender: t.senderId,
+    receiver: t.receiverId,
+  }));
+}
