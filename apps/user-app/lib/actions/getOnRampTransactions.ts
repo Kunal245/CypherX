eefb5b1146de6afce517948aa5598986ac5c52ce
Kunal <killerkunal45@gmail.com>
2026-05-28 09:28:55 +0000

    fix: type error

--- apps/user-app/lib/actions/getOnRampTransactions.ts
+++ apps/user-app/lib/actions/getOnRampTransactions.ts
@@ -12,7 +12,7 @@ export async function getOnRampTransactions() {
   const txns = await db.onRampTransaction.findMany({
     where: { userId: Number(session?.user?.id) },
   });
-  return txns.map((t) => ({
+  return txns.map((t: any) => ({
     time: t.startTime,
     amount: t.amount,
     status: t.status,
