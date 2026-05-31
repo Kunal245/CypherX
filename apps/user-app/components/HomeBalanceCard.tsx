f57550831dbf59cb9ab6af25ff22db1e7e9fc76d
Kunal <killerkunal45@gmail.com>
2026-05-31 19:52:12 +0000

    adding hide balance action

--- apps/user-app/components/HomeBalanceCard.tsx
+++ apps/user-app/components/HomeBalanceCard.tsx
@@ -1,15 +1,17 @@
-// "use client"
+"use client"
 
 
 import { Button } from "@workspace/ui/components/button";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@workspace/ui/components/card";
 import { Wallet } from "lucide-react";
 import Link from "next/link";
+import { useState } from "react";
 // import { useRouter } from "next/navigation";
 
 export const HomeBalanceCard = ({ amount, locked }: { amount: number; locked: number }) => {
 
   // const router = useRouter()
+  const [hideBalance, setHideBalance] = useState(true)
 
   return (
     <Card className="w-full max-w-full bg-primary/60 bg-gradient-to-br from-secondary/20 via-primary/20 to-secondary/20">
@@ -20,10 +22,9 @@ export const HomeBalanceCard = ({ amount, locked }: { amount: number; locked: nu
         </CardTitle>
       </CardHeader>
       <CardContent className="space-y-2">
+        <Button onClick={() => {setHideBalance(!hideBalance)}} >click</Button>
         <div className="flex justify-between py-2">
-          <span className="text-3xl font-bold tracking-tight">
-            ₹ {(locked + amount) / 100} INR
-          </span>
+          <span className="text-3xl font-bold tracking-tight">{hideBalance ? "🙊" : `₹ ${(locked + amount) / 100} INR`}</span>
         </div>
       </CardContent>
       <CardFooter>
