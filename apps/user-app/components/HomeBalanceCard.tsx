22db6952136fbdcdfff22a9bd281b65edc7622cd
Kunal <killerkunal45@gmail.com>
2026-05-31 18:36:41 +0000

    added gradient to homecard

--- apps/user-app/components/HomeBalanceCard.tsx
+++ apps/user-app/components/HomeBalanceCard.tsx
@@ -7,7 +7,7 @@ import { Wallet } from "lucide-react";
 
 export const HomeBalanceCard = ({ amount, locked }: { amount: number; locked: number }) => {
   return (
-    <Card className="w-full max-w-full bg-primary/60 bg-gradient-to-br from-secondary/20 via-primary/50 to-primary">
+    <Card className="w-full max-w-full bg-primary/60 bg-gradient-to-br from-secondary/20 via-primary/20 to-secondary/20">
       <CardHeader>
         <CardTitle className="flex items-center gap-2">
           <Wallet className="h-5 w-5" />
@@ -24,7 +24,8 @@ export const HomeBalanceCard = ({ amount, locked }: { amount: number; locked: nu
       <CardFooter>
         <div className="flex justify-between py-2">
           <span className="text-3xl font-bold tracking-tight">
-            <Button variant="secondary" className="bg-primary/90 border-primary">Send</Button>
+            <Button variant="default" className="">Send</Button>
+            {/* <Button variant="secondary" className="bg-primary/90 border-primary">Recieave</Button> */}
           </span>
         </div>
       </CardFooter>
