7b634db728dfb68ce6f75a3fbd2b908b5188c2ee
Kunal <killerkunal45@gmail.com>
2026-06-03 21:12:04 +0000

    testing signing

--- apps/user-app/app/signin/page.tsx
+++ apps/user-app/app/signin/page.tsx
@@ -2,6 +2,8 @@
 
 
 import { Input } from "@workspace/ui/components/input";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import { useState } from "react"
 
 
@@ -10,13 +12,31 @@ import { useState } from "react"
 export default async function Signin() {
 
     const [number, setNumber] = useState("");
-    const [password, setPassword] = useState();
+    const [password, setPassword] = useState("");
+    const router = useRouter();
+
+
+    await signIn('credentials', {
+      redirect: false,
+      number,
+      password,
+    })
+      .then((response:any) => {
+        console.log(response);
+        router.replace('/dashboard');
+      })
+      .catch((error:any) => {
+        console.log(error);
+      });
 
     return <div>
         signin
 
         <Input placeholder="number" onChange={(e) => {
             setNumber(e.target.value)
+        }} ></Input>
+        <Input placeholder="password" onChange={(e) => {
+            setPassword(e.target.value)
         }} ></Input>
     </div>
 }
