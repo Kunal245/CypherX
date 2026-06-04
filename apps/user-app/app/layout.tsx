c174c6c5f6da323f04360f642f786ddc2a5cfb0d
Kunal <killerkunal45@gmail.com>
2026-06-04 19:56:49 +0000

    fixed appbar

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
     >
       <Providers>
         <body>
-          <AppbarClient></AppbarClient>
+          {/* <AppbarClient></AppbarClient> */}
           <ThemeProvider>{children}</ThemeProvider>
         </body>
       </Providers>
