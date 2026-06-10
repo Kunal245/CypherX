bea989fab6f9a8c80ea48c352b638bcd2f7ef57e
Kunal <killerkunal45@gmail.com>
2026-06-10 07:51:55 +0000

    getDetails action added

--- apps/user-app/app/(user)/(dashboard)/details/page.tsx
+++ apps/user-app/app/(user)/(dashboard)/details/page.tsx
@@ -1,8 +1,14 @@
+import { getDetails } from "@/lib/actions/getDetails"
 
 
 
 
 export default async function details(){
+
+
+    const user = await getDetails();
+
+
     return <div>
         Account Page soon...
 
